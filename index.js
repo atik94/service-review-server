3a791880116248786b2030a10ff93be20ef284da
atik94 <atikurrahmanatik94@gmail.com>
2022-11-09 20:50:12 +0600

    single user review query

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ async function run() {
       res.send(service);
     });
     //Review api
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
     app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
